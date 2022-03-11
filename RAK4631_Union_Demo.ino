a613c3f2537e731b5459d37149d9265314527dbe
Kongduino <dbvic@me.com>
2022-03-11 10:34:11 +0800

    Added comments to the code

--- RAK4631_Union_Demo.ino
+++ RAK4631_Union_Demo.ino
@@ -1,13 +1,14 @@
 #include <ArduinoJson.h>
 #include <Adafruit_TinyUSB.h>
+// Necessary to get Serial
 
-union S {
-  float n[3];
-  uint8_t c[12];
+union ThreeValues {
+  float n[3]; // 3 float values
+  uint8_t c[12]; // 3 x 4 bytes = 12 bytes total
+  // These are parallel declarations, not consecutive
 };
 
-char buff[128];
-S s;
+char buff[128]; // Too much probably but let's play it safe
 char myJson[] = "{\"temp\":26.5,\"hm\":62.3,\"hpa\":1016.20}";
 
 void setup() {
@@ -25,34 +26,39 @@ void setup() {
     while (1);
   }
   Serial.println("=====================================================================\n");
-  s.n[0] = doc["temp"];
-  s.n[1] = doc["hm"];
-  s.n[2] = doc["hpa"];
-  sprintf(buff, "temp = %.2f\n", s.n[0]);
+  ThreeValues tv; // An instance of our union.
+  ThreeValues ov = {0.0, 0.0, 0.0}; // Another instance, zeroed out
+  // Assign the 3 float values to their respective variables
+  tv.n[0] = doc["temp"];
+  tv.n[1] = doc["hm"];
+  tv.n[2] = doc["hpa"];
+  // Let's make a copy of the raw bytes from tv to ov
+  memcpy(ov.c, tv.c, 12);
+
+  sprintf(buff, "temp = %.2f\n", tv.n[0]);
   Serial.print(buff);
-  S v = {0.0, 0.0, 0.0};
-  memcpy(v.c, s.c, 12);
-  sprintf(buff, "check temp = %.2f\n", v.n[0]);
+  sprintf(buff, "check temp = %.2f\n", ov.n[0]);
   Serial.print(buff);
 
-  sprintf(buff, "humidity = %.2f\n", s.n[1]);
+  sprintf(buff, "humidity = %.2f\n", tv.n[1]);
   Serial.print(buff);
-  sprintf(buff, "check humidity = %.2f\n", v.n[1]);
+  sprintf(buff, "check humidity = %.2f\n", ov.n[1]);
   Serial.print(buff);
 
-  sprintf(buff, "pressure = %.2f HPa\n", s.n[2]);
+  sprintf(buff, "pressure = %.2f HPa\n", tv.n[2]);
   Serial.print(buff);
-  sprintf(buff, "check pressure = %.2f HPa\n", v.n[2]);
+  sprintf(buff, "check pressure = %.2f HPa\n", ov.n[2]);
   Serial.print(buff);
 
   memset(buff, 0, 128);
-  Serial.println("s:");
-  for (uint8_t i = 0; i < 12; i++) sprintf(buff + (i * 3), "%02x ", s.c[i]);
+  Serial.println("Original:");
+  // Quick hexdump
+  for (uint8_t i = 0; i < 12; i++) sprintf(buff + (i * 3), "%02x ", tv.c[i]);
   Serial.println(buff);
 
   memset(buff, 0, 128);
-  Serial.println("v:");
-  for (uint8_t i = 0; i < 12; i++) sprintf(buff + (i * 3), "%02x ", v.c[i]);
+  Serial.println("Copy:");
+  for (uint8_t i = 0; i < 12; i++) sprintf(buff + (i * 3), "%02x ", ov.c[i]);
   Serial.println(buff);
 }
 
